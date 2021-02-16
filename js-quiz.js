43ab4f4ad4e6eef8aec5601177cb16c378b19c5f
Pasquali Marco <pasquali21707@iistorriani.it>
2021-02-17 00:34:30 +0100

    jsquiz

---
+++ js-quiz.js
@@ -0,0 +1,17 @@
+﻿  function verifica(){
+	var do1=document.form.d1.value;
+	var do2=document.form.d2.value;
+	var do3=document.form.d3.value;
+	var giuste=0;
+	if(do1==3){
+		giuste++;
+		}
+	if(do2==1){
+		giuste++;
+		}
+	if(do3==4){
+		giuste++;
+		}
+	var sbagliate=3-giuste;
+	alert("Risposte corrette: "+giuste+"\n Risposte sbagliate: "+sbagliate);
+	}
