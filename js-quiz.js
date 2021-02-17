c5d6a0ce064ae17ce77959f076265f3c480c197e
Pasquali Marco <pasquali21707@iistorriani.it>
2021-02-17 01:48:38 +0100

    Accorgimenti pt3

--- js-quiz.js
+++ js-quiz.js
@@ -2,6 +2,9 @@
 	var do1=document.form.d1.value;
 	var do2=document.form.d2.value;
 	var do3=document.form.d3.value;
+	var do4=document.form.d4.value;
+	var do5=document.form.d5.value;
+	var do6=document.form.d6.value;
 	var giuste=0;
 	if(do1==3){
 		giuste++;
@@ -12,6 +15,16 @@
 	if(do3==4){
 		giuste++;
 		}
-	var sbagliate=3-giuste;
+	if(do4==2){
+		giuste++;
+	}
+	if(do5==3){
+		giuste++;
+	}
+	if(do6==3){
+		giuste++;
+	}
+		
+	var sbagliate=6-giuste;
 	alert("Risposte corrette: "+giuste+"\n Risposte sbagliate: "+sbagliate);
 	}
